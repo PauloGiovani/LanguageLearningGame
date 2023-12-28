b534bb350f12d471512339c911232f685fecffa1
Paulo Giovani <pg_faria@yahoo.com.br>
2023-12-28 17:40:13 -0300

    Context and Checking Answer

--- js/script.js
+++ js/script.js
@@ -17,13 +17,22 @@ let app = {
         // Obtém o elemento de todas as alternativas
         let alts = document.querySelectorAll('.alternative');
 
+        // Ao invés de uma Arrow Function, poderíamos utilizar o .bind
+        // alts.forEach(function(element, index){
+        //   // Listener para capturar o clique
+        //   element.addEventListener('click', function(){
+        //     // Verifica se a alternativa está correta
+        //     this.checkAnswer(index);
+        //   }.bind(this));
+        // }.bind(this));
+
         // Percorre cada alternativa
-        alts.forEach(function(element, index) {
+        alts.forEach((element, index) => {
             // Listener para capturar o clique
-            element.addEventListener('click', function() {
+            element.addEventListener('click', () => {
                 // Verifica se a alternativa está correta
-                console.log("Check Correct Answer");
-            })
+                this.checkAnswer(index);
+            });
         });
 
         // Exibe a primeira questão
@@ -32,6 +41,9 @@ let app = {
 
     // Método para exibir a primeira questão
     showQuestion: function(q) {
+        // Mantém um controle da questão atual
+        this.currQuestion = q;
+
         // Seleciona o elemento para o título
         let titleDiv = document.getElementById('title');
 
@@ -46,8 +58,20 @@ let app = {
             // Exibe o texto de cada alternativa
             element.textContent = q.alternatives[index];
         });
+    },
+
+    // Método para checar se a alternativa está correta
+    checkAnswer: function(userSelected) {
+        // Questão correta
+        if(this.currQuestion.correctAnswer == userSelected) {
+            console.log("Correct Answer");
+        }
+        // Questão incorreta
+        else {
+            console.log("Wrong Answer");
+        }
     }
-    
+
 };
 
 // -----------------------------------------------------------------------------
