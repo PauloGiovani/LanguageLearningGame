ef1972d1a63aaef6b198afda556b50c085c4fb62
Paulo Giovani <pg_faria@yahoo.com.br>
2023-12-28 16:42:23 -0300

    Handling User Input

--- js/script.js
+++ js/script.js
@@ -21,10 +21,30 @@ function showQuestion(q) {
 
     // 4. Altera o valor de cada alternativa
     alts.forEach(function(element, index) {
+        // Altera o texto da alternativa
         element.textContent = q.alternatives[index];
+        
+        // Listener para capturar o clique em cada alternativa
+        element.addEventListener('click', function() {
+            // Verifica se a alternativa está correta
+            if(q.correctAnswer == index) {
+                console.log("Correct Answer!");
+            }
+            else {
+                console.log("Wrong Answer!");
+            }
+        })
     });
 
 }
 
 // Exibe a questão e suas alternativas
 showQuestion(question);
+
+// Seleciona o botão
+let btn = document.getElementById('btn');
+
+// Listener para o botão: click
+btn.addEventListener('click', function() {
+    console.log('Clicked!');
+});
