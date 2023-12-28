e0408ca0eae235250807e496435a0d8ccefd6ebe
Paulo Giovani <pg_faria@yahoo.com.br>
2023-12-28 17:12:59 -0300

    App Structure – Part 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+// -----------------------------------------------------------------------------
 // Objeto para representar uma questão em JavaScript
+// -----------------------------------------------------------------------------
 let question = {
     title: 'gato',
     alternatives: ['dog', 'cat', 'bird', 'fish'],
@@ -6,44 +8,49 @@ let question = {
 };
 
 // -----------------------------------------------------------------------------
-// Função para inicializar a aplicação
+// Objeto para gerenciar a aplicação
 // -----------------------------------------------------------------------------
-function start() {
-    // Obtém o elemento de todas as alternativas
-    let alts = document.querySelectorAll('.alternative');
+let app = {
 
-    // Percorre cada alternativa
-    alts.forEach(function(element, index) {
-        // Listener para capturar o clique
-        element.addEventListener('click', function() {
-            // Verifica se a alternativa está correta
-            console.log("Check Correct Answer");
-        })
-    });
+    // Método para inicializar a aplicação
+    start: function() {
+        // Obtém o elemento de todas as alternativas
+        let alts = document.querySelectorAll('.alternative');
 
-    // Exibe a primeira questão
-    showQuestion(question);
-}
+        // Percorre cada alternativa
+        alts.forEach(function(element, index) {
+            // Listener para capturar o clique
+            element.addEventListener('click', function() {
+                // Verifica se a alternativa está correta
+                console.log("Check Correct Answer");
+            })
+        });
+
+        // Exibe a primeira questão
+        this.showQuestion(question);
+    },
+
+    // Método para exibir a primeira questão
+    showQuestion: function(q) {
+        // Seleciona o elemento para o título
+        let titleDiv = document.getElementById('title');
+
+        // Altera o título
+        titleDiv.textContent = q.title;
+
+        // Obtém o elemento de todas as alternativas
+        let alts = document.querySelectorAll('.alternative');
+
+        // Percorre cada alternativa
+        alts.forEach(function(element, index) {
+            // Exibe o texto de cada alternativa
+            element.textContent = q.alternatives[index];
+        });
+    }
+    
+};
 
 // -----------------------------------------------------------------------------
-// Função para exibir as questões
-// -----------------------------------------------------------------------------
-function showQuestion(q) {
-    // Seleciona o elemento para o título
-    let titleDiv = document.getElementById('title');
-
-    // Altera o título
-    titleDiv.textContent = q.title;
-
-    // Obtém o elemento de todas as alternativas
-    let alts = document.querySelectorAll('.alternative');
-
-    // Percorre cada alternativa
-    alts.forEach(function(element, index) {
-        // Exibe o texto de cada alternativa
-        element.textContent = q.alternatives[index];
-    });
-}
-
 // Inicializa a aplicação
-start();
+// -----------------------------------------------------------------------------
+app.start();
